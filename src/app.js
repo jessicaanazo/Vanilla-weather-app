202ba75828f0e4b7f9ceeed32559a9cbb9c88e47
Jessica Anazo <jessicaanazo@yahoo.com>
2022-09-24 01:14:12 +0100

    Added the relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -29,16 +29,20 @@ function showTemperature(response) {
   let humidityElement = document.querySelector('#humidity');
    let windElement = document.querySelector('#wind');
    let dateElement = document.querySelector('#date');
+   let iconElement = document.querySelector('#icon');
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute('src', `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 let apiKey = '8b7c83c1d2550cda541e73c93abe2c90';
- let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=${apiKey}&units=metric`;
+let city = 'Lagos'
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
  axios.get(apiUrl).then(showTemperature);
